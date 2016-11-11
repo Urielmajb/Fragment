73f0d25fca3f64bd3f0e5c964eaa681c2cd07bfc
Bryant <broxis94@gmail.com>
2016-11-11 17:46:27 -0600

    Initial commit

---
+++ app/src/main/java/mamg/ejemplo/fragment/PrimerFragment.java
@@ -0,0 +1,23 @@
+package mamg.ejemplo.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrador on 11/11/2016.
+ */
+public class PrimerFragment extends Fragment{
+    public PrimerFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
